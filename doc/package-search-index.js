0b1c8687767fd2d03dd96e4acdaa6261cf54e6b8
naoki-takata <naokibtc@gmail.com>
2025-09-14 16:19:22 +0900

    add JavaDoc index

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"example.csv2json"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"example.csv2json","l":"Csv2JsonCli"},{"p":"example.csv2json","l":"CsvToJsonCore"},{"p":"example.csv2json","l":"CsvToJsonCore.Options"}];updateSearchResults();
